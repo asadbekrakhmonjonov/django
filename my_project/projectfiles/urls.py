208ac1b8137ebb30cc200fbdbef680b1523f7533
Asadbek <k@Kui-MacBookAir.local>
2024-02-29 15:33:25 +0900

    //

--- my_project/projectfiles/urls.py
+++ my_project/projectfiles/urls.py
@@ -4,6 +4,7 @@ from .views import *
 urlpatterns = [
     path('', views.signPage),
     path('home/', views.homePage, name='home'),
+    path('login/', views.loginPage, name='login'),
     path('task_page/', views.taskPage, name="task_page"),
     path('update_task/<str:pk_test>/', views.updateTask, name="update_task"),
     path('deleteTask/<str:pk>/', views.deleteTask, name="delete_task"),
--- my_project/projectfiles/views.py
+++ my_project/projectfiles/views.py
@@ -1,4 +1,4 @@
-from .filter import TaskFilter
+from django.contrib.auth import authenticate, login
 from .forms import CreateUserForm, TaskForm
 from django.shortcuts import render, redirect
 from .filter import TaskFilter
@@ -7,15 +7,33 @@ from .models import Task
 
 def signPage(request):
     form = CreateUserForm()
+
     if request.method == 'POST':
         form = CreateUserForm(request.POST)
         if form.is_valid():
             form.save()
-            messages.success(request, 'You signed up')
+            user = form.cleaned_data.get('username')
+            messages.success(request, 'Account is created for', user)
             # Use a named URL if available, replace 'home' with your actual home page name
-            return redirect('home')
+            return redirect('login')
     context = {'regForm': form}
     return render(request, 'projectfiles/register.html', context)
+
+def loginPage(request):
+
+    if request.method == 'POST':
+
+        email = request.POST.get('email')
+        password = request.POST.get('password')
+        user = authenticate(email=email, password=password)
+        if user is not None:
+
+            login(request, user)
+            return redirect('home')
+        else:
+            messages.info(request, 'Email or Password is incorrect')
+    context = {}
+    return render(request, 'projectfiles/login.html', context)
 
 def taskPage(request):
     form = TaskForm()
