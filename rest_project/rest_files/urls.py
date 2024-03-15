8eb9bbbb0be012eb1582411e29db14b91dc3e31d
Asadbek <k@Kui-MacBookAir.local>
2024-03-15 14:18:55 +0900

    jwt tokens

--- rest_project/rest_files/urls.py
+++ rest_project/rest_files/urls.py
@@ -1,7 +1,14 @@
 from django.urls import path
 from . import views
+from rest_framework_simplejwt.views import (
+    TokenObtainPairView,
+    TokenRefreshView,
+)
 urlpatterns = [
-    path('', views.endpoints),
-    path('advocates/', views.advocate_list, name='advocates'),
+    path('', views.Endpoints.as_view()),
+    path('token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
+    path('advocates/', views.AdvocateList.as_view()),
     path('advocate/<str:username>/', views.AdvocateDetail.as_view()),
+    path('companies/', views.CompaniesList.as_view()),
+
 ]
--- rest_project/rest_files/views.py
+++ rest_project/rest_files/views.py
@@ -1,33 +1,32 @@
 from django.shortcuts import render, redirect
 from django.http import JsonResponse
-from rest_framework.decorators import api_view
+from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
-from .models import Advocate
-from .serializers import AdvocateSerializer
+from .models import Advocate, Company
+from .serializers import AdvocateSerializer, CompanySerializer
 from django.db.models import Q
 from rest_framework.views import APIView
+from rest_framework.permissions import IsAuthenticated
+
 # Create your views here.
-@api_view(['GET'])
-def endpoints(request):
-    data = ['/advocates', '/advocates/: username']
-    return Response(data)
-@api_view(['GET', 'POST'])
-def advocate_list(request):
-    if request.method == 'GET':
-        query = request.GET.get('query')
-
-        if query is None:
-            query = ''
-
-        advocates = Advocate.objects.filter(Q(username__icontains=query) | Q( bio__icontains=query))
+class Endpoints(APIView):
+    def get(self, request):
+        data = ['/advocates', '/advocates/: username']
+        return Response(data)
+@permission_classes([IsAuthenticated])
+class AdvocateList(APIView):
+    def get(self, request):
+        query = request.query_params.get('query', '')
+        advocates = Advocate.objects.filter(Q(username__icontains=query) | Q(bio__icontains=query))
         serializer = AdvocateSerializer(advocates, many=True)
         return Response(serializer.data)
 
-    if request.method == 'POST':
-
-        advocate = Advocate.objects.create(username=request.data['username'], bio=request.data['bio'])
-        serializer = AdvocateSerializer(advocate, many=False)
-        return Response(serializer.data)
+    def post(self, request):
+        serializer = AdvocateSerializer(data=request.data)
+        if serializer.is_valid():
+            serializer.save()
+            return Response(serializer.data, status=201)
+        return Response(serializer.errors, status=400)
 
 class AdvocateDetail(APIView):
     def get_object(self, username):
@@ -35,7 +34,7 @@ class AdvocateDetail(APIView):
         try:
             return Advocate.objects.get(username=username)
         except Advocate.DoesNotExist:
-            raise Advocate
+            raise JsonResponse("Advocate does not exist!")
 
 
     def get(self, request, username):
@@ -58,26 +57,8 @@ class AdvocateDetail(APIView):
         advocate.delete()
 
         return Response('user was deleted')
-""""@api_view(['GET', 'PUT', 'DELETE'])
-def advocate_detail(request, username):
-
-    advocate = Advocate.objects.get(username=username)
-
-    if request.method == 'GET':
-        serializer = AdvocateSerializer(advocate, many=False)
-
+class CompaniesList(APIView):
+    def get(self,request):
+        companies = Company.objects.all()
+        serializer = CompanySerializer(companies, many=True)
         return Response(serializer.data)
-
-    if request.method == 'PUT':
-
-        advocate.username = request.data['username']
-        advocate.bio = request.data['bio']
-
-        advocate.save()
-        serializer = AdvocateSerializer(advocate, many=False)
-
-        return Response(serializer.data)
-    if request.method == 'DELETE':
-        advocate.delete()
-        serializer = AdvocateSerializer(advocate, many=False)
-        return Response('user was deleted')"""
